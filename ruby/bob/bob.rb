b7aa3ebb72ff10b907c30daad4a85bb4c57d6335
Javier Julio <jjfutbol@gmail.com>
2013-09-07 18:48:37 -0400

    Simplify by allowing the statement check to be the default fallback if no match found.

--- ruby/bob/bob.rb
+++ ruby/bob/bob.rb
@@ -1,27 +1,16 @@
 class Bob
 
   def hey(message)
-    result = nil
-    if silence?(message)
-      result = 'Fine. Be that way!'
-    end
-    if statement?(message)
-      result = 'Whatever.'
-    end
-    if asking_question?(message)
-      result = 'Sure.'
-    end
-    if shouting?(message)
-      result = 'Woah, chill out!'
-    end
-    result
+    return 'Fine. Be that way!' if silence?(message)
+    return 'Woah, chill out!' if shouting?(message)
+    return 'Sure.' if question?(message)
+    'Whatever.'
   end
 
   protected
 
-    def asking_question?(message)
-      # TODO: check for multiple sentences (! ), (. ), etc.
-      message && message.end_with?('?') #&& message.match(/^(! ){1,}(. ){1,}+\?$/)
+    def question?(message)
+      message && message.end_with?('?')
     end
 
     def statement?(message)
